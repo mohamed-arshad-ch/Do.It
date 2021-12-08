e62e2d276b6a98bad320bb8a46a96fb5f5ede657
lukman <vvlukman6@gmail.com>
2021-12-08 12:33:55 +0530

    database name changed

--- config/db-config.js
+++ config/db-config.js
@@ -1,6 +1,6 @@
 const dbUrl = process.env.DB_URL
 const port = process.env.DB_PORT
 module.exports = {
-   myurl: 'mongodb://localhost:27017/test',
+   myurl: 'mongodb://localhost:27017/money_manager',
    secret: 'mysecretkey'  //going to be used later on for key token
 }
